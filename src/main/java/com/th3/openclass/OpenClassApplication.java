fe00d5c8b5bab59b1358f1edc9ad3281d6b53097
Ayoub <elpasha117@gmail.com>
2023-01-24 18:53:19 +0100

    init

---
+++ src/main/java/com/th3/openclass/OpenClassApplication.java
@@ -0,0 +1,13 @@
+package com.th3.openclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/th3/openclass/OpenClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th3.openclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
