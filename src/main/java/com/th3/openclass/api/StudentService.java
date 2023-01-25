e66702e74c72bd5e009bf5a8d39923185b8db8ec
Ayoub <elpasha117@gmail.com>
2023-01-25 21:38:38 +0100

    init

--- src/main/java/com/th3/openclass/api/StudentService.java
+++ src/main/java/com/th3/openclass/api/StudentService.java
@@ -2,61 +2,28 @@ package com.th3.openclass.api;
 
 import com.th3.openclass.model.Student;
 import com.th3.openclass.repository.StudentRepository;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
-@RestController
-@RequestMapping
+@Service
 public class StudentService
 {
     private final StudentRepository studentRepository;
-
-    record NewStudentRequest(
-            Long apogeeNum,
-            String firstName,
-            String lastName,
-            String email,
-            Integer age,
-            String avatarUrl,
-            Date birthDate,
-            Date enrolDate,
-            Date exprDate
-    )
-    { }
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
-//
-//    @GetMapping("/")
-//    public String index()
-//    {
-//        return "index";
-//    }
-//    @PostMapping("/register")
-//    public String studentRegistration(@ModelAttribute Student student)
-//    {
-//        studentRepository.save(student);
-//        return "index";
-//    }
-    @GetMapping("home")
     public List<Student> getStudent()
     {
         return studentRepository.findAll();
     }
 
-    @PostMapping
-    public void addStudent(@RequestBody NewStudentRequest newStudentRequest)
+    public void addStudent(Student student)
     {
-        Student student = new Student();
-        student.setEmail(newStudentRequest.email);
         studentRepository.save(student);
     }
-
-
-
 }
+
+
 
--- src/main/java/com/th3/openclass/controller/StudentController.java
+++ src/main/java/com/th3/openclass/controller/StudentController.java
@@ -1,6 +1,61 @@
 package com.th3.openclass.controller;
 
+import com.th3.openclass.api.StudentService;
+import com.th3.openclass.model.Student;
+import com.th3.openclass.repository.StudentRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/v1")
 public class StudentController
 {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService)
+    {
+        this.studentService = studentService;
+    }
+     record NewStudentRequest(
+            Long apogeeNum,
+            String firstName,
+            String lastName,
+            String email,
+            Integer age,
+            String avatarUrl,
+            Date birthDate,
+            Date enrolDate,
+            Date exprDate
+    )
+    { }
+
+    @PostMapping
+    public void addStudent(@RequestBody final String email)
+    {
+        Student student = new Student((long)15, "ayoub","azizi",email);
+        studentService.addStudent(student);
+    }
+
+    @GetMapping("get")
+    public List<Student> getStudent()
+    {
+        return studentService.getStudent();
+    }
+
+//    @GetMapping("/")
+//    public String index()
+//    {
+//        return "index";
+//    }
+//    @PostMapping("/register")
+//    public String studentRegistration(@ModelAttribute Student student)
+//    {
+//        studentRepository.save(student);
+//        return "index";
+
+//    }
 
 }
